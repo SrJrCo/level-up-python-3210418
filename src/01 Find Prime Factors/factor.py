077696701c5239e7fbd361b71d7dcc9312adcb74
Jonrae Belvis <73924511+SrJrCo@users.noreply.github.com>
2023-05-04 18:24:31 +0000

    Factor program fin

--- src/01 Find Prime Factors/factor.py
+++ src/01 Find Prime Factors/factor.py
@@ -1,16 +1,17 @@
-def get_prime_factors(number):
-    factors = []
-    divisor = 2
-    while divisor <= number:
-        if number % divisor == 0:
-            factors.append(divisor)
-            number = number // divisor
-        else:
-            divisor += 1
+def get_prime_factors(): # create function to get the prime factors of numbers
+    factors = [] # Create list of the factors so we can pull
+    divisor = 2 # divisor var 
+    while divisor <= number: # 2 <= everthing above 2
+    if number%divisor == 0: # first find if the number is divisible by 2
+        factors.append(divisor) # if it is append that the divors
+        number = number // divisor
+    else: 
+        divisor += 1
     return factors
 
 
 # commands used in solution video for reference
+
 if __name__ == '__main__':
     print(get_prime_factors(630))  # [2, 3, 3, 5, 7]
     print(get_prime_factors(13))  # [13]
